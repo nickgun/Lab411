2bd4868520dac6c7e545b0003d858a29e271c93b
nickgun <tnt.fet@gmail.com>
2013-10-03 13:52:13 +0700

    Rename Project

---
+++ EEGProject/src/com/eegproject/MainActivity.java
@@ -0,0 +1,18 @@
+package com.eegproject;
+
+import com.egproject.R;
+
+import android.R.string;
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+	}
+}
+
