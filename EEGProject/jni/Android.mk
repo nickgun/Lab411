6eb98d70b242c74209e2912a7c779bccd3ff9d42
nickgun <tnt.fet@gmail.com>
2013-12-04 15:16:00 +0700

    update 31/11/2013

--- EEGProject/jni/Android.mk
+++ EEGProject/jni/Android.mk
@@ -2,5 +2,5 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE    := ProcessData
-LOCAL_SRC_FILES := main.c edf2ascii.c fastICA.c matrix.c svdcmp.c
+LOCAL_SRC_FILES := main.c ./ReadData/edf2ascii.c ./ICAfastICA.c matrix.c ./ICA/svdcmp.c
 include $(BUILD_SHARED_LIBRARY)
--- EEGProject/src/com/eegproject/MainActivity.java
+++ EEGProject/src/com/eegproject/MainActivity.java
@@ -1,22 +1,84 @@
 package com.eegproject;
 
-import android.os.Bundle;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Scanner;
+
 import android.app.Activity;
-import android.view.Menu;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
-
+	TextView tvCompData;
+	Button btnChooseEdfFile;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
+		tvCompData = (TextView)findViewById(R.id.tvCompData);
+		btnChooseEdfFile = (Button)findViewById(R.id.btnChooseEdfFile);
+		
+		btnChooseEdfFile.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				//choose file edf
+				Intent myIntent = new Intent(Intent.ACTION_GET_CONTENT);
+				myIntent.setType("file/*");
+				startActivityForResult(myIntent, 1);
+			}
+		});
 	}
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
+	public void onActivityResult(int requestCode, int resultCode, Intent data) {
+		if (resultCode == RESULT_OK) {
+			if (requestCode == 1) {
+				String path = data.getData().getPath();
+				Log.e("TAG", "\nPATH= "+path);
+				long start_time = System.currentTimeMillis();
+				int i = runProcessData();
+				long stop_time = System.currentTimeMillis();
+				long time = (stop_time - start_time)/1000;
+				Log.e("TAG", "\nSignal: " + i + " sample");
+				tvCompData.setText("Thoi gian thu tin hieu: " + (i/128) + "s\n");
+				tvCompData.append("Thoi gian xu ly tin hieu: "+ time +"s");
+			}
+		}
 	}
+	
+	private void ReadEdfFile(String path){
+		File fl = new File(path);
+		FileInputStream ips = null;
+		Scanner input = null;
+		if (fl.exists()) {
+			try {
+				ips = new FileInputStream(fl);
+				input = new Scanner(ips);
+				{
+					while (input.hasNextLine()) {
+						String line = input.nextLine();
+						String[] arrStr = line.split("\\$");
+						
+					}
+				}
+				ips = null;
+				input = null;
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		} else {
+		}
+	}
+	
+	public native int runProcessData();
 
+	static{
+		System.loadLibrary("ProcessData");
+	}
 }
